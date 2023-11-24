e5e8c2411371413cb7fd5a93850956296f0f770d
Exe <exequiel@qualitando.com>
2023-11-24 19:18:48 +0100

    fix NET 8 features

--- Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace NextDesk.App;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
